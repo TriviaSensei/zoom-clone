1106c0c6b530ea01da545ab3bd640148878bf14c
Chuck <charles.yu4@gmail.com>
2022-07-02 13:34:11 -0400

    adding activity log for debugging ios

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,7 @@ const log = document.getElementById('activity-log');
 
 const myPeer = new Peer(undefined);
 let myUserId;
+let videos = [];
 
 const logActivity = (msg) => {
 	const line = document.createElement('li');
@@ -20,23 +21,7 @@ const myStream =
 	navigator.webkitGetUserMedia ||
 	navigator.mozGetUserMedia;
 
-myPeer.on('call', (call) => {
-	logActivity('call received');
-	setTimeout(() => {
-		myStream({ video: true, audio: true }, (stream) => {
-			call.answer(stream);
-			const video = document.createElement('video');
-			video.setAttribute('playsinline', true);
-
-			call.on('stream', (remoteStream) => {
-				addVideoStream(video, remoteStream, call.peer);
-			});
-		}),
-			(err) => {
-				console.log('failed to get local stream', err);
-			};
-	}, 1000);
-});
+let myVideoStream;
 
 navigator.mediaDevices
 	.getUserMedia({
@@ -44,6 +29,7 @@ navigator.mediaDevices
 		audio: true,
 	})
 	.then((stream) => {
+		myVideoStream = stream;
 		addVideoStream(myVideo, stream, myUserId);
 		logActivity('adding my own stream');
 		myPeer.on('call', (call) => {
@@ -60,9 +46,37 @@ navigator.mediaDevices
 		});
 	});
 
+myPeer.on('call', (call) => {
+	logActivity('call received');
+
+	call.answer(myVideoStream);
+	const video = document.createElement('video');
+	video.setAttribute('playsinline', true);
+
+	call.on('stream', (remoteStream) => {
+		logActivity('stream received');
+		if (!document.getElementById(`video-${call.peer}`)) {
+			addVideoStream(video, remoteStream, call.peer);
+		}
+	});
+	// myStream({ video: true, audio: true }, (stream) => {
+	// 	call.answer(stream);
+	// 	const video = document.createElement('video');
+	// 	video.setAttribute('playsinline', true);
+
+	// 	call.on('stream', (remoteStream) => {
+	// 		addVideoStream(video, remoteStream, call.peer);
+	// 	});
+	// }),
+	// 	(err) => {
+	// 		console.log('failed to get local stream', err);
+	// 	};
+});
+
 myPeer.on('open', (userId) => {
 	myUserId = userId;
 	document.getElementById('user-id').innerHTML = userId;
+
 	socket.emit('join-room', {
 		roomId,
 		userId,
@@ -77,46 +91,69 @@ socket.on('user-disconnected', (data) => {
 	logActivity(`user ${data.userId} disconnected`);
 	const v = document.getElementById(`video-${data.userId}`);
 	if (v) {
+		videos = videos.filter((v) => {
+			return v !== data.userId;
+		});
+		logActivity(`removing video ${data.userId}`);
 		v.remove();
 	}
 });
 
 const addVideoStream = (video, stream, id) => {
+	if (videos.includes(id)) {
+		return;
+	}
+	videos.push(id);
+	const videoId = id || 'me';
+	logActivity(`adding video ${id === undefined ? '(me)' : id}`);
 	video.srcObject = stream;
 	video.addEventListener('loadedmetadata', () => {
 		video.play();
 	});
-	video.setAttribute('id', `video-${id}`);
+	video.setAttribute('id', `video-${videoId}`);
 	videoGrid.appendChild(video);
 };
 
 const connectToNewUser = (userId, stream) => {
-	const getUserMedia =
-		navigator.getUserMedia ||
-		navigator.webkitGetUserMedia ||
-		navigator.mozGetUserMedia;
+	logActivity(`calling ${userId} with my stream`);
+	const call = myPeer.call(userId, myVideoStream);
+	const video = document.createElement('video');
+	video.setAttribute('playsinline', true);
+	call.on('stream', (theirStream) => {
+		logActivity('their stream received');
+		// addVideoStream(video, theirStream);
+		addVideoStream(video, theirStream, userId);
+	});
+	call.on('close', () => {
+		logActivity('call closed');
+		video.remove();
+	});
 
-	getUserMedia(
-		{
-			video: true,
-			audio: true,
-		},
-		(stream) => {
-			logActivity(`calling ${userId} with my stream`);
-			const call = myPeer.call(userId, stream);
-			const video = document.createElement('video');
-			video.setAttribute('playsinline', true);
-			call.on('stream', (theirStream) => {
-				// addVideoStream(video, theirStream);
-				addVideoStream(video, theirStream, userId);
-			});
-			call.on('close', () => {
-				logActivity('call closed');
-				video.remove();
-			});
-		},
-		(err) => {
-			console.log('failed to get local stream', err);
-		}
-	);
+	// const getUserMedia =
+	// 	navigator.getUserMedia ||
+	// 	navigator.webkitGetUserMedia ||
+	// 	navigator.mozGetUserMedia;
+	// getUserMedia(
+	// 	{
+	// 		video: true,
+	// 		audio: true,
+	// 	},
+	// 	(stream) => {
+	// 		logActivity(`calling ${userId} with my stream`);
+	// 		const call = myPeer.call(userId, stream);
+	// 		const video = document.createElement('video');
+	// 		video.setAttribute('playsinline', true);
+	// 		call.on('stream', (theirStream) => {
+	// 			// addVideoStream(video, theirStream);
+	// 			addVideoStream(video, theirStream, userId);
+	// 		});
+	// 		call.on('close', () => {
+	// 			logActivity('call closed');
+	// 			video.remove();
+	// 		});
+	// 	},
+	// 	(err) => {
+	// 		console.log('failed to get local stream', err);
+	// 	}
+	// );
 };
